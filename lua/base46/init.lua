edc9d237e6e394941bb9d4fafa620025170170e7
Max Miliano <milianordelcanto@gmail.com>
2026-03-05 18:00:36 -0300

    feat: plugins option

--- lua/base46/init.lua
+++ lua/base46/init.lua
@@ -1,5 +1,24 @@
 local groups = require 'base46.groups'
 
+---@class base46.plugins
+---@field bufferline boolean
+---@field nvimtree boolean
+---@field neotree boolean
+---@field lspconfig boolean
+---@field telescope boolean
+---@field dashboard boolean
+---@field gitsigns boolean
+---@field dired boolean
+---@field cmp boolean
+---@field navic boolean
+---@field markview boolean
+---@field modeline boolean
+---@field hydra boolean
+---@field rainbow boolean
+---@field mason boolean
+---@field flutterTools boolean
+---@field supermaven boolean
+
 ---@class base46.groups
 ---@field name string
 ---@field val vim.api.keyset.highlight
@@ -9,6 +28,7 @@ local groups = require 'base46.groups'
 ---@field transparency? boolean
 ---@field themes string[]
 ---@field groups base46.groups[]
+---@field plugins base46.plugins
 
 ---@class base46.init
 local M = {}
@@ -19,6 +39,25 @@ M.config = {
   transparency = false,
   themes = {},
   groups = {},
+  plugins = {
+    bufferline = true,
+    nvimtree = true,
+    neotree = true,
+    lspconfig = true,
+    telescope = true,
+    dashboard = true,
+    gitsigns = true,
+    dired = true,
+    cmp = true,
+    navic = true,
+    markview = true,
+    modeline = true,
+    hydra = true,
+    rainbow = true,
+    mason = true,
+    flutterTools = true,
+    supermaven = true,
+  },
 }
 
 ---@param opts base46.config
