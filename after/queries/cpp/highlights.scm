4a668b22999ac7d01e18d38383cf429b8cb45ffb
Max Miliano <milianordelcanto@gmail.com>
2026-03-19 17:49:50 -0300

    feat(queries): cpp

---
+++ after/queries/cpp/highlights.scm
@@ -0,0 +1,6 @@
+; extends
+(parameter_declaration
+    (placeholder_type_specifier) @type.builtin (#set! "priority" 125))
+
+(parameter_declaration
+    (placeholder_type_specifier (type_identifier) @type (#set! "priority" 125)))
