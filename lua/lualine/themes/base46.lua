fd8588de134aa6b51e698180cb8631e743432415
Max Miliano <milianordelcanto@gmail.com>
2026-06-08 23:43:12 -0300

    refactor(lualine): fg color

--- lua/lualine/themes/base46.lua
+++ lua/lualine/themes/base46.lua
@@ -1,23 +1,26 @@
 ---@base46.palette
 local c = vim.g.base46_palette
+local color = require 'base46.color'
+local shade = color.shade
+
 local lualine = {
   normal = {
-    a = { fg = c.base_30.base01, bg = c.blue, gui = 'bold' },
+    a = { fg = shade(c.blue, 5), bg = c.blue, gui = 'bold' },
     b = { fg = c.base_16.base01, bg = c.base_30.base04 },
     c = { fg = c.base_30.base04, bg = c.base_16.base01 },
     z = { fg = c.base_30.base01, bg = c.blue },
   },
   insert = {
-    a = { fg = c.base_30.base01, bg = c.green },
+    a = { fg = shade(c.green, 5), bg = c.green },
   },
   visual = {
-    a = { fg = c.base_30.base01, bg = c.magenta },
+    a = { fg = shade(c.magenta, 5), bg = c.magenta },
   },
   replace = {
-    a = { fg = c.base_30.base01, bg = c.red },
+    a = { fg = shade(c.red, 5), bg = c.red },
   },
   command = {
-    a = { fg = c.base_30.base01, bg = c.orange },
+    a = { fg = shade(c.orange, 5), bg = c.orange },
   },
 }
 return lualine
